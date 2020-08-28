8aedcb141ba612f89ddd1fc6e80874a251c8c521
Jose Robles <engjoserobles@gmail.com>
2020-08-28 17:24:29 -0400

    Updates

---
+++ precache-manifest.e7c1f5be1574d8f13d6bc505bc8fb6ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20bc61c52c72825318c78cf6f8658a1a",
+    "url": "/m-r/index.html"
+  },
+  {
+    "revision": "d9fe7f4bcddbc9e67f9b",
+    "url": "/m-r/static/css/main.88236af3.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/m-r/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/m-r/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d9fe7f4bcddbc9e67f9b",
+    "url": "/m-r/static/js/main.40922b85.chunk.js"
+  },
+  {
+    "revision": "f2f2ca7f69b34aef1377",
+    "url": "/m-r/static/js/runtime-main.08337808.js"
+  }
+]);
